512c691670f1093f685e66364562c06f0ae4899d
L0uka <loukacadaulino@gmail.com>
2023-05-22 15:20:22 +0200

    first API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,18 @@
 let send = document.getElementById("send");
-let cityname = document.getElementById("search").value;
-
-send.addEventListener("click", weather);
 
 async function weather() {
+  let cityname = document.getElementById("search").value;
+
   let url = await fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=5&appid=00dde611c283263cbabba55f2e6247f0`
+    `http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=1&appid=00dde611c283263cbabba55f2e6247f0`
   );
   let data = await url.json();
 
+  console.log(cityname);
   console.log(url);
   console.log(data);
+  console.log(data[0].lat);
+  console.log(data[0].lon);
 }
-console.log(cityname);
+
+send.addEventListener("click", weather);
