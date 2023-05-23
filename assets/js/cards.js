725a7393cc848243c259628bc387827086731156
L0uka <loukacadaulino@gmail.com>
2023-05-23 11:26:16 +0200

    ajout de =! div pour chaque données

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -1,47 +1,59 @@
 export function displayCard(data) {
-  let date = data.list[0].dt_txt;
-  let dateP = document.createElement("p");
-  dateP.textContent = date;
-  weatherData.appendChild(dateP);
+  let weatherDataContainer = document.getElementById("weatherData");
+  let weatherCardsWrapper = document.createElement("div");
 
-  let weatherCode = data.list[0].weather[0].icon;
-  let weatherIcoIMG = document.createElement("img");
-  let weatherIco = `https://openweathermap.org/img/wn/${weatherCode}@2x.png`;
-  weatherIcoIMG.src = weatherIco;
-  weatherData.appendChild(weatherIcoIMG);
+  for (let i = 0; i < data.list.length; i++) {
+    let weatherCardDiv = document.createElement("div");
+    weatherCardDiv.classList.add("weather-card");
 
-  let weather = data.list[0].weather[0].main;
-  let weatherP = document.createElement("p");
-  weatherP.textContent = weather;
-  weatherData.appendChild(weatherP);
+    let date = data.list[i].dt_txt;
+    let dateP = document.createElement("p");
+    dateP.textContent = date;
+    weatherCardDiv.appendChild(dateP);
 
-  let weatherDes = data.list[0].weather[0].description;
-  let weatherDesP = document.createElement("p");
-  weatherDesP.textContent = weatherDes;
-  weatherData.appendChild(weatherDesP);
+    let weatherCode = data.list[i].weather[0].icon;
+    let weatherIcoIMG = document.createElement("img");
+    let weatherIco = `https://openweathermap.org/img/wn/${weatherCode}@2x.png`;
+    weatherIcoIMG.src = weatherIco;
+    weatherCardDiv.appendChild(weatherIcoIMG);
 
-  let max = data.list[0].main.temp_max;
-  let maxP = document.createElement("p");
-  maxP.textContent = "Temperature max: " + max;
-  weatherData.appendChild(maxP);
+    let weather = data.list[i].weather[0].main;
+    let weatherP = document.createElement("p");
+    weatherP.textContent = weather;
+    weatherCardDiv.appendChild(weatherP);
 
-  let min = data.list[0].main.temp_min;
-  let minP = document.createElement("p");
-  minP.textContent = "Temperature min: " + min;
-  weatherData.appendChild(minP);
+    let weatherDes = data.list[i].weather[0].description;
+    let weatherDesP = document.createElement("p");
+    weatherDesP.textContent = weatherDes;
+    weatherCardDiv.appendChild(weatherDesP);
 
-  let pressure = data.list[0].main.pressure;
-  let pressureP = document.createElement("p");
-  pressureP.textContent = "pressure: " + pressure;
-  weatherData.appendChild(pressureP);
+    let max = data.list[i].main.temp_max;
+    let maxP = document.createElement("p");
+    maxP.textContent = "Temperature max: " + max;
+    weatherCardDiv.appendChild(maxP);
 
-  let humidity = data.list[0].main.humidity;
-  let humidityP = document.createElement("p");
-  humidityP.textContent = "humidity: " + humidity;
-  weatherData.appendChild(humidityP);
+    let min = data.list[i].main.temp_min;
+    let minP = document.createElement("p");
+    minP.textContent = "Temperature min: " + min;
+    weatherCardDiv.appendChild(minP);
 
-  let speedWind = data.list[0].wind.speed;
-  let speedWindP = document.createElement("p");
-  speedWindP.textContent = "speed wind: " + speedWind;
-  weatherData.appendChild(speedWindP);
+    let pressure = data.list[i].main.pressure;
+    let pressureP = document.createElement("p");
+    pressureP.textContent = "Pressure: " + pressure;
+    weatherCardDiv.appendChild(pressureP);
+
+    let humidity = data.list[i].main.humidity;
+    let humidityP = document.createElement("p");
+    humidityP.textContent = "Humidity: " + humidity;
+    weatherCardDiv.appendChild(humidityP);
+
+    let speedWind = data.list[i].wind.speed;
+    let speedWindP = document.createElement("p");
+    speedWindP.textContent = "Wind speed: " + speedWind;
+    weatherCardDiv.appendChild(speedWindP);
+
+    weatherCardsWrapper.appendChild(weatherCardDiv);
+  }
+
+  weatherDataContainer.appendChild(weatherCardsWrapper);
 }
