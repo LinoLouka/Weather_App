4dea7e109fd505a41328da6df1afec18eaa6d4b0
L0uka <loukacadaulino@gmail.com>
2023-05-23 09:25:35 +0200

    add info for creation elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 let send = document.getElementById("send");
+let weatherData = document.getElementsByClassName("weatherData");
 const apiKey = "00dde611c283263cbabba55f2e6247f0";
 
 async function weather() {
@@ -19,11 +20,19 @@ function display(lat, lon) {
   let url = fetch(
     `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`
   );
-  console.log(lat);
-  console.log(lon);
+  //   console.log(lat);
+  //   console.log(lon);
   url
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+      //   console.log(data.list[0].main.temp_max);
+      //   console.log(data.list[0].main.temp_min);
+      //   console.log(data.list[0].main.presure);
+      //   console.log(data.list[0].main.humidity);
+      //   console.log(data.list[0].wind.speed);
+      //   console.log(data.list[0].weather.main);
+      //   console.log(data.list[0].weather.description);
+      //   console.log(data.list[0].weather.icon);
     });
 }
